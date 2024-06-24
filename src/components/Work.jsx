91011c47f09591ebfd96ad74f7a1fc8051ce898e
rewky05 <apur.rosales.swu@phinmaed.com>
2024-06-24 20:06:13 +0800

    new project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WorkImg from '../assets/projects/workImg.jpeg'
 import WashUp from '../assets/projects/washUp.jpg'
+import SKStaCruz from '../assets/projects/skStaCruz.jpg'
 
 const Work = () => {
   return (
@@ -18,9 +19,9 @@ const Work = () => {
                 className='shadow-lg shadow-gray-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
                     {/* Hover Effect */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 text-center'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            Amanviro Website
+                            Amanviro
                         </span>
                         <div className='pt-8 text-center'>
                             <a href='https://rodcanete.github.io/' target="_blank" rel="noopener noreferrer">
@@ -37,15 +38,34 @@ const Work = () => {
                 className='shadow-lg shadow-gray-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
                     {/* Hover Effect */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 text-center'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            Wash Up Laundry Website
+                            Wash Up Laundry
                         </span>
                         <div className='pt-8 text-center'>
                             <a href='https://laundry-washup.netlify.app/' target="_blank" rel="noopener noreferrer">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href='https://github.com/rewky05/washup-laundry-new' target="_blank" rel="noopener noreferrer">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a> 
+                            
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage: `url(${SKStaCruz})`}} 
+                className='shadow-lg shadow-gray-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/* Hover Effect */}
+                    <div className='opacity-0 group-hover:opacity-100 text-center'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            SK Santa Cruz
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='https://sk-sta-cruz.vercel.app/' target="_blank" rel="noopener noreferrer">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a> 
                             
